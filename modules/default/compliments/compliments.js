b1c78ff5f9a8e03db091cc2c1b3c6a6d7d825c0c
toe7685 <kpittillo7685@gmail.com>
2024-04-01 19:52:06 -0500

    ghkgk

--- modules/default/compliments/compliments.js
+++ modules/default/compliments/compliments.js
@@ -8,11 +8,7 @@ Module.register("compliments", {
 	// Module config defaults.
 	defaults: {
 		compliments: {
-			anytime: ["Hey there sexy!"],
-			morning: ["Good morning, handsome!", "Enjoy your day!", "How was your sleep?"],
-			afternoon: ["Hello, beauty!", "You look sexy!", "Looking good today!"],
-			evening: ["Wow, you look hot!", "You look nice!", "Hi, sexy!"],
-			"....-01-01": ["Happy new year!"]
+			anytime: ["Shut Up Brady", "Hi Lindsey. You look lovely", "Hi Everly", "Hello Harper Poop-butt?"],
 		},
 		updateInterval: 30000,
 		remoteFile: null,
